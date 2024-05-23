83e997abe344f6b1c263f83bc2e18d321b96b3bb
sed-i <82407168+sed-i@users.noreply.github.com>
2024-05-22 23:41:04 -0400

    Use clap to choose output type

--- README.md
+++ README.md
@@ -2,35 +2,48 @@
 
 Convert a juju bundle to a mermaid diagram.
 
+## Install
+```bash
+cargo install --git https://github.com/sed-i/juju-graph
+```
 
-## Run from source
+## Usage
+Convert bundle to mermaid:
+```shell
+juju export-bundle | juju-graph mermaid
+```
+
+Render a mermaid.live image url:
+```shell
+juju export-bundle | juju-graph mermaid --url
+```
+
+Render svg from graphviz:
+```shell
+juju export-bundle | juju-graph graphviz | dot -Tsvg > bundle.svg 
+```
+
+
+## Development
+### Run from source
 
 ```bash
-# Read "bundle.yaml"
+# Read from "bundle.yaml"
 cargo run
 
-# Pass as stdin
+# Pass via stdin
 cargo run < bundle.yaml
 
 # or
 juju export-bundle | cargo run
 ```
 
-## Install from source dir
+### Install from source dir
 ```bash
 cargo install --path .
 juju-graph < bundle.yaml
 ```
 
-## Install via repo url
-```bash
-cargo install --git https://github.com/sed-i/juju-graph
-```
-
-## Render svg from graphviz
-```bash
-juju-graph < bundle.yaml | dot -Tsvg > bundle.svg
-```
 
 ## References
 - https://github.com/mermaid-js/mermaid-live-editor/discussions/1291
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use std::error::Error;
 use std::io::IsTerminal;
+use clap::{Parser, Subcommand};
 
 mod bundle;
 mod string_utils;
 
 use bundle::Bundle;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Mermaid {
+        #[arg(long)]
+        url: bool,
+    },
+    Graphviz {},
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = std::io::stdin();
@@ -18,10 +35,22 @@ fn main() -> Result<(), Box<dyn Error>> {
         serde_yaml::from_reader(input.lock())?
     };
 
-    //println!("{}", bundle.to_mermaid());
-    println!("{}", bundle.to_graphviz());
-    //println!("{}", bundle.to_img_url());
-    //println!("{}", bundle.to_edit_url());
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Mermaid { url}) => {
+            if *url {
+                // println!("{}", bundle.to_edit_url());
+                println!("{}", bundle.to_img_url());
+            } else {
+                println!("{}", bundle.to_mermaid());
+            }
+        }
+        Some(Commands::Graphviz {} ) => {
+            println!("{}", bundle.to_graphviz());
+        }
+        None => {}
+    }
 
     Ok(())
 }
